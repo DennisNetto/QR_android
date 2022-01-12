5ffa28bb36592659d2da45820bb747f853b0937b
jimmy <workemaileg45@gmail.com>
2022-01-12 02:42:38 -0700

    Sends the qr message to server

--- app/src/main/java/com/example/qr_forqr/MainActivity.java
+++ app/src/main/java/com/example/qr_forqr/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,10 +14,63 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 // implements onClickListener for the onclick behaviour of button
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     Button scanBtn;
     TextView messageText, messageFormat;
+    private Socket client;
+    private PrintWriter printwriter;
+    private EditText textField;
+    private Button button;
+    private String message;
+
+    class ClientThread implements Runnable {
+        private final String message;
+
+        ClientThread(String message) {
+            this.message = message;
+        }
+        @Override
+        public void run() {
+            try {
+                // the IP and port should be correct to have a connection established
+                // Creates a stream socket and connects it to the specified port number on the named host.
+                client = new Socket("3.21.205.75", 5050);  // connect to server
+                BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                printwriter = new PrintWriter(client.getOutputStream(),true);
+                printwriter.write(message);  // write the message to output stream
+                printwriter.flush();
+                String fromServer;
+                while ((fromServer = in.readLine()) != null) {
+                    textField.setText(fromServer);
+                    if (fromServer.equals("Msg received")) {
+
+                        break;
+                    }
+                }
+
+
+                printwriter.close();
+                in.close();
+
+
+                // closing the connection
+                client.close();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,8 +111,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             } else {
                 // if the intentResult is not null we'll set
                 // the content and format of scan message
-                messageText.setText(intentResult.getContents());
+                String text = intentResult.getContents();
+                messageText.setText(text);
                 messageFormat.setText(intentResult.getFormatName());
+                new Thread(new ClientThread(text)).start();
+
             }
         } else {
             super.onActivityResult(requestCode, resultCode, data);
